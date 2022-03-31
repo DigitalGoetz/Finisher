ea03a48b51ac49801aea0385f36dc61d7662705d
Michael Goetz <digital.goetz@gmail.com>
2022-03-31 12:25:20 -0400

    threw together some basic docs

--- run_random.py
+++ run_random.py
@@ -3,13 +3,13 @@ import random
 import time
 import uuid
 
-useRandom = str(os.environ.get("RANDOM", "true")).lower()
+useRandom = str(os.environ.get("RANDOM", "yes")).lower()
 timeLimit = int(os.environ.get("LIMIT", "20"))
 
 id = uuid.uuid4()
 print(f"{id}: Running")
 
-if useRandom == "true":
+if useRandom == "yes":
     duration = random.randint(1, timeLimit)
 else:
     duration = timeLimit
